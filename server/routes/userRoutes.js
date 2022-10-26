6ea5490906febf0888ce18f144a6ed14b84acefd
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-26 19:45:16 +0600

    Install Packages

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
-router.post("/register", (req, res) => {
+const registerValidation = [
+  body("name").not().isEmpty().withMessage("Name is required!"),
+];
+
+router.post("/register", registerValidation, (req, res) => {
+  const errors = validationResult(req);
+
+  // Delete Later
   console.log(req.body);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 });
 
 module.exports = router;
