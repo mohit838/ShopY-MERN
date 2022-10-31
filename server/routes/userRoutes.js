1612d82b5b836e5d01b9410b68c95a6537eaff09
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-31 15:33:08 +0600

    Update comment

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,8 +4,11 @@ const {
   registerValidation,
   loginValidations,
 } = require("../validations/userValidations");
+
+// Express Route Initialized
 const router = express.Router();
 
+// User Reg and Login Routes
 router.post("/register", registerValidation, registration);
 router.post("/login", loginValidations, login);
 
