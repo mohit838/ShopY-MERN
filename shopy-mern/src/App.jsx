e0653b65507ac96bd0fcfbb1f4e497b6dc658dc2
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-11-02 06:32:07 +0600

    Create Sales Page

--- shopy-mern/src/App.jsx
+++ shopy-mern/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Hero } from "./component";
-import { heroapi } from "./data/data.js";
+import { heroapi, Sales } from "./data/data.js";
 
 function App() {
   return (
     <>
       <main>
         <Hero heroapi={heroapi} />
+        <Sales />
       </main>
     </>
   );
